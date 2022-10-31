1e0c40183fbedd44539a0383b820555fc9321ce6
SamuelLuisGomes <samuelluisgomestech@gmail.com>
2022-10-31 00:26:03 -0300

    Termino do curso de Sintaxe e Tipos de dados em C#

--- Pottencial .NET Developer/Conhecendo.NET/Fundamentos/Program.cs
+++ Pottencial .NET Developer/Conhecendo.NET/Fundamentos/Program.cs
@@ -1,7 +1,29 @@
 ﻿using Fundamentos.Models; // Caminho referente a pasta Models e a class Pessoa está dentro dessa pasta.
-Pessoa p = new Pessoa(); // instanciando uma nova pessoa.
 
-p.Nome = "Samuel Gomes"; // Adicionando o Nome
-p.idade = 18; // Adicionando a Idade
-p.Apresentar(); // Aqui estou chamando o método para apresentar as informações da Pessoa.
+// ---Tipo DataTime---
+DateTime dataAtual = DateTime.Now; // Declarando uma DateTime
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm")); // Configurando o formato do DateTime 
+
+
+
+
+// ---Tipos de Dados---
+// string apresentacao  = "Olá, seja bem vindo!";
+// int qtd = 1;
+// double altura = 0.80;
+// decimal dolar = 1.80M;
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variável quantidade: " + qtd);
+// Console.WriteLine("Valor da variável altura: " + altura);
+// Console.WriteLine("Valor da variável dolar: " + dolar);
+// Console.WriteLine("Valor da variável condição: " + condicao);
+
+// ---Criando um pessoa---
+// Pessoa p = new Pessoa(); // instanciando uma nova pessoa.
+
+// p.Nome = "Samuel Gomes"; // Adicionando o Nome
+// p.idade = 18; // Adicionando a Idade
+// p.Apresentar(); // Aqui estou chamando o método para apresentar as informações da Pessoa.
 
