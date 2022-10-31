345e187223be62d9a301f58fd4bde3176b8de605
SamuelLuisGomes <samuelluisgomestech@gmail.com>
2022-10-30 23:39:35 -0300

    Terminando de estudar sintaxe e identação no C#

--- Pottencial .NET Developer/Conhecendo.NET/Fundamentos/Models/Pessoa.cs
+++ Pottencial .NET Developer/Conhecendo.NET/Fundamentos/Models/Pessoa.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 namespace Fundamentos.Models
 {
+
     public class Pessoa
     {
          // Criando variáveis/atributos
@@ -12,7 +13,7 @@ namespace Fundamentos.Models
         public int idade { get; set; }
 
         // Criando métodos
-        public void apresentar(){
+        public void Apresentar(){
             // Se fizermos dessa forma podemos concatenar as variáveis e o texto.
             // Basta inserirmos o $ antes das aspas e com isso podemos colocar as variáveis entre colchetes.
             Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {idade} anos!");
--- Pottencial .NET Developer/Conhecendo.NET/Fundamentos/Program.cs
+++ Pottencial .NET Developer/Conhecendo.NET/Fundamentos/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Teste");
+﻿using Fundamentos.Models; // Caminho referente a pasta Models e a class Pessoa está dentro dessa pasta.
+Pessoa p = new Pessoa(); // instanciando uma nova pessoa.
+
+p.Nome = "Samuel Gomes"; // Adicionando o Nome
+p.idade = 18; // Adicionando a Idade
+p.Apresentar(); // Aqui estou chamando o método para apresentar as informações da Pessoa.
+
